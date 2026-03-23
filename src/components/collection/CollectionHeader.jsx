32d86184d9f972f3a81d4b7c35ea60f15c5eb1d0
sum097 <safimian2002@gmail.com>
2026-03-23 13:29:12 -0600

    fixed loading skeleton state

--- src/components/collection/CollectionHeader.jsx
+++ src/components/collection/CollectionHeader.jsx
@@ -5,25 +5,9 @@ import Skeleton from "../ui/Skeleton.jsx";
 export default function CollectionHeader({ collection, loading }) {
   if (loading) {
     return (
-      <header id="collection-header">
+      <header id="collection-header" style={{ backgroundColor: "#e5e5e5" }}>
         <div className="row collection-header__row">
-          <div className="collection-header__content">
-            <div className="collection-header__left">
-              <Skeleton width="80px" height="80px" borderRadius="12px" />
-              <Skeleton width="200px" height="2rem" borderRadius="6px" />
-              <Skeleton width="100px" height="1rem" borderRadius="6px" />
-            </div>
-            <div className="collection-header__right">
-              <div className="collection-header__columns">
-                {new Array(5).fill(0).map((_, i) => (
-                  <div key={i} className="collection-header__column">
-                    <Skeleton width="80px" height="1.2rem" borderRadius="4px" />
-                    <Skeleton width="60px" height="0.8rem" borderRadius="4px" />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+          <div className="collection-header__content"></div>
         </div>
       </header>
     );
@@ -46,7 +30,12 @@ export default function CollectionHeader({ collection, loading }) {
               className="collection-header__img"
             />
             <div className="collection-header__name">{collection.title}</div>
-            <Link to={`/user/${collection.creatorId}`} className="collection-header__author">{collection.creator}</Link>
+            <Link
+              to={`/user/${collection.creatorId}`}
+              className="collection-header__author"
+            >
+              {collection.creator}
+            </Link>
           </div>
           <div className="collection-header__right">
             <div className="collection-header__columns">
@@ -60,7 +49,10 @@ export default function CollectionHeader({ collection, loading }) {
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">{parseFloat(collection.floor).toFixed(2)}</span> ETH
+                  <span className="semibold">
+                    {parseFloat(collection.floor).toFixed(2)}
+                  </span>{" "}
+                  ETH
                 </span>
                 <span className="collection-header__column__label">
                   Floor price
@@ -68,7 +60,10 @@ export default function CollectionHeader({ collection, loading }) {
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">{parseFloat(collection.bestOffer).toFixed(2)}</span> ETH
+                  <span className="semibold">
+                    {parseFloat(collection.bestOffer).toFixed(2)}
+                  </span>{" "}
+                  ETH
                 </span>
                 <span className="collection-header__column__label">
                   Best offer
